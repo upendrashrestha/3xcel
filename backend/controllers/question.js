e6664ee965b4b6a508ea21015d0d955aeb01fc64
Upendra Shrestha <upendrashrestha1@gmail.com>
2021-01-09 09:31:40 -0600

    question api updated

--- backend/controllers/question.js
+++ backend/controllers/question.js
@@ -1,26 +1,26 @@
 const Question = require('../models/questions');
 
 exports.getIndex = async (req, res) => {
-    const question = await Question.find((data) => data);
+     await Question.find((data) => data).then((question)=>
 
-    try {
-         console.log(question);
+     {
+         //console.log(question);
         // res.status(200).render('index', { question: question });
         res.json(question);
-    } catch (error) {
-        console.log(error);
-    }
+    }). catch ((err) =>{
+        console.log(err);
+        res.status(401).send({error:err, message:'No FAQ selected.'});
+    });
 };
 
 exports.getQuestion = async (req, res) => {
     const questionId = req.params.questionId;
-    const question = await Question.findById(questionId, (question) => question);
-    try {
-        console.log(question);
+    await Question.findById(questionId, (question) => question).then((question) =>{
         res.status(200).render('question', { question: question });
-    } catch (error) {
-        console.log(error);
-    }
+    }).catch ((err) => {
+        console.log(err);
+        res.status(401).send({error:err, message:'No FAQ selected.'});
+    });
 };
 
 exports.getAddQuestion = (req, res) => {
@@ -33,31 +33,36 @@ exports.getEditQuestion = async (req, res) => {
     const editMode = req.query.edit;
 
     if (!editMode) {
-        return res.redirect('/');
+        res.status(401).send({ message:'Invalid request.'})
+  
     }
 
-    const question = await Question.findById(questionId);
-
-    try {
-        if (!questionId) {
-            return res.redirect('/');
-        }
-        console.log(question);
-        res.status(200).render('edit-question', { question: question, editing: editMode });
-    } catch (error) {
-        console.log(error);
+    if (!questionId) {
+        res.status(401).send({ message:'No question selected.'})
     }
+
+     await Question.findById(questionId).then((question)=>{
+        
+        res.status(200).send('edit-question', { question: question, editing: editMode });
+    }).
+    catch((err) => {
+        res.status(401).send({error:err, message:'No FAQ updated.'});
+    });
 };
 
 exports.postQuestion = (req, res) => {
-    console.log(req);
+   // console.log(req);
+   try{
     const { question, answer, category } = req.body.model;
 
     const q = new Question({ question: question, category: category, answer: answer });
     q.save();
-    console.log('Question Added to the database');
+    console.log('FAQ add successfully');
    // res.status(200);
-   res.status(201).redirect('/');
+   res.status(201).send('FAQ added successfully.');
+   }catch(err){
+       res.status(401).send({error:err, message:'No FAQ added.'});
+   }
 };
 
 exports.postEditQuestion = (req, res) => {
@@ -67,27 +72,24 @@ exports.postEditQuestion = (req, res) => {
             q.question = question;
             q.answer = answer;
             q.category = category;
-
             return q.save();
         })
         .then(() => {
-            console.log('Item Updated');
-            res.status(201).redirect('/');
+            res.status(201).send({message:'FAQ updated successfully.'});
         })
         .catch((err) => {
             console.log(err);
+            res.status(403).send({message:'Error : No FAQ updated.', error: err});
         });
 };
 
 exports.postDelete = async (req, res) => {
     const questionId = req.body.model;
-    const question = await Question.findByIdAndRemove(questionId, (data) => data);
-
-    try {
-        console.log(question);
+    await Question.findByIdAndRemove(questionId, (data) => data).then((question)=>{
         console.log('Item Deleted');
-        res.redirect('/');
-    } catch (error) {
-        console.log(error);
-    }
+        res.status(201).send({message:'FAQ deleted successfully.'});
+    }).catch((err) => {
+        console.log(err);
+        res.status(403).send({message:'Error : No FAQ deleted.', error: err});
+    });
 };
